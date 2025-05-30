e66eb12b9818f7d71fe6b0dc608ddcdd36c21c65
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-05-30 17:03:27 +0500

    ass

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useRouteError} from 'react-router-dom'
+import img from '../assets/Без названия (1).jpg'
 
 const Error = () => {
-  return (
+
+  const error = useRouteError()
+  if(error.status === 404) 
+    return (
     <section className="section-center">
-      <h2 className="title">404 - Page Not Found</h2>
+      <div className="title">
+        <h2 className="title">404 - Page Not Found</h2>
+        <div className="title-underline"></div>
+      </div>
+      <img src={img} alt="404" className="img" />
       <div className="title-underline"></div>
-      <p className="text">Sorry, the page you tried cannot be found.</p>
+      <p className="text">Siz notogri yuldasiz</p>
       <Link to="/" className="btn">Go Back Home</Link>
     </section>
   )
-}
+} 
 
 export default Error
