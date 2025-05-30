f7d1fddcc36699c51de91ebdebfe330e58226812
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-05-30 13:35:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { Landing,
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Newsletter
+} from './pages'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
