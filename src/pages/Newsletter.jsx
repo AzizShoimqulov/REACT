6c2d9497d9e1e5471cc3eaf539fe5cf94b57ca9a
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-05-30 17:09:18 +0500

    add

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Newsletter = () => {
+  const [searchLetter, setSearchLetter] = useState('')
+  const [cocktails, setCocktails] = useState([])
+
+  useEffect(() => {
+    if (searchLetter) {
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchLetter}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setCocktails(data.drinks || [])
+        })
+    }
+  }, [searchLetter])
+
   return (
-    <section className="">
+    <section>
       <form className="form">
         <h4>Subscribe to Newsletter</h4>
         <div className="form-control">
@@ -13,7 +26,22 @@ const Newsletter = () => {
           />
           <button type="submit" className="btn">Subscribe</button>
         </div>
+        <input
+          type="text"
+          placeholder="Enter a letter"
+          value={searchLetter}
+          onChange={(e) => setSearchLetter(e.target.value)}
+          maxLength="1"
+        />
       </form>
+      <div className="cocktail-list">
+        {cocktails.map((drink) => (
+          <div key={drink.idDrink}>
+            <h5>{drink.strDrink}</h5>
+            <img src={drink.strDrinkThumb} alt={drink.strDrink} width="100" />
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
